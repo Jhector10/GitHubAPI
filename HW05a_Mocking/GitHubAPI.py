c40f98783a105a594a8d47ce26a9dd77923eea73
Jhector10 <jashsmash10@gmail.com>
2021-03-12 21:18:25 -0500

    for mocking

---
+++ HW05a_Mocking/GitHubAPI.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def getRepos(userID):
+    count = 0
+    repos = []
+    url = "https://api.github.com/users/" + userID + "/repos"
+    request = requests.get(url)
+    data = request.json()
+    
+    for repo in data:
+        urlCommits = "https://api.github.com/repos/" + userID + "/" + repo["name"] + "/commits"
+        requestCommits = requests.get(urlCommits)
+        count += 1
+        commits = len(requestCommits.json())
+        repos.append({"Repo": repo["name"], "Commits": commits})
+        print("Repo: " + repo["name"] + " Number of commits: " + str(commits))
+        if count == 2:
+            break  
+
+    git_info = {
+        "id": userID,
+        "repoCount": count,
+        "repos": repos
+    }
+    
+    return git_info
+
+userID = "Jhector10"
+getRepos(userID)
---
+++ HW05a_Mocking/test-api.py
@@ -0,0 +1,55 @@
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr
+@author: rk
+"""
+
+import unittest
+from unittest.mock import patch, Mock
+
+from GitHubAPI import getRepos
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestApi(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    git_info = {
+        "id": "Jhector10",
+        "repoCount": 5,
+        "repos": [
+            {
+                'Repo': 'ClassifyTriangles',
+                 "Commits": 9
+            },
+            {
+                'Repo': 'GitHubAPI',
+                'Commits': 15
+            }
+        ]
+    }
+
+    def testUserID(self): 
+        with patch('GitHubAPI.getRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.git_info
+        self.assertEqual(getRepos("Jhector10")["id"], 'Jhector10')
+
+    def testRepoCount(self): 
+        with patch('GitHubAPI.getRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.git_info
+        self.assertEqual(getRepos("Jhector10")["repoCount"], 2)
+        
+    def testFirstRepo(self): 
+        with patch('GitHubAPI.getRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.git_info
+        self.assertEqual(getRepos("Jhector10")["repos"][0], {"Repo": "ClassifyTriangles", "Commits": 9})
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
